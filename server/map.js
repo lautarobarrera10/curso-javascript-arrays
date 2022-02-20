b9db97ea5b04ad25c906e37e5851b5a244546c78
Lautaro <lautarobarrera12@gmail.com>
2022-02-19 21:21:26 -0300

    map reloaded

--- server/map.js
+++ server/map.js
@@ -13,3 +13,40 @@ console.log(letters, newsLetters);
 // Con map
 const newsLetters2 = letters.map(letter => letter + '+');
 console.log(newsLetters2);
+
+
+// Map reloaded
+const products = [
+    {
+        name: 'Play Station 3',
+        price: 100,
+    },
+    {
+        name: 'TV',
+        price: 120,
+    },
+    {
+        name: 'PC',
+        price: 300,
+    },
+    {
+        name: 'Laptop',
+        price: 250,
+    },
+];
+
+// Acá usamos map para hacer limpieza de datos y separar los precios en un nuevo array para su posterior uso
+const productPrices = products.map(product => product.price);
+console.log(productPrices);
+
+// Acá usamos map para crear un nuevo array donde se incluya el impuesto de cada producto.
+const productsWithTaxes = products.map(product => {
+    tax = product.price * 0.66;
+    return {
+        ...product,
+        tax
+    }
+})
+
+console.log(productsWithTaxes);
+console.log(products);
